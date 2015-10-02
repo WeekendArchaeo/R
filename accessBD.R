1e3c48e066cb8656ee2e1688f31e23ae4936f9f5
Ana Cruz Martín <anacruzmartin@gmail.com>
2015-10-02 16:28:54 +0200

    Update accessBD.R

--- accessBD.R
+++ accessBD.R
@@ -1,5 +1,7 @@
 # This script is based on the R-bloggers post at
 # http://www.r-bloggers.com/load-postgis-geometries-in-r-without-rgdal/
+#
+# The script must be invoked using source("complete path to this script")
 
 # Install packages (comment if they are previously installed)
 install.packages("RPostgreSQL")
@@ -13,3 +15,4 @@ library("sp")
 # Create connection with Postgres db
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv,dbname='yourDBname',host='localhost',port=5432,user='yourUser',password='yourPw')
+
