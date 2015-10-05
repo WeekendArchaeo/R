7848597d74d3551a94ff61831beeb8c9115c8048
Ana Cruz Martín <anacruzmartin@gmail.com>
2015-10-05 13:13:21 +0200

    Update accessBD.R

--- accessBD.R
+++ accessBD.R
@@ -1,6 +1,12 @@
-# This script is based on the R-bloggers post at
+# ----------------------------------
+# Accesing a PosgGIS database from R
+# ----------------------------------
+
+# This script is based on the askubuntu post at
+# http://askubuntu.com/questions/312294/once-installed-geos-library-c-and-c-and-then-trying-to-install-rgeos-packa
+# and the R-bloggers post at
 # http://www.r-bloggers.com/load-postgis-geometries-in-r-without-rgdal/
-#
+
 # The script must be invoked using source("complete path to this script")
 
 # Install packages (comment if they are previously installed)
